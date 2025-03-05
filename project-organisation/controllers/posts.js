dd1b956ef3219c88e19abc128ca02020a438628c
Razvan Andrei <chiper_raizy@yahoo.com>
2025-03-05 12:36:15 +0100

    adding controllers

---
+++ project-organisation/controllers/posts.js
@@ -0,0 +1,74 @@
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.findAll({ include: User });
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const createPost = async (req, res) => {
+  try {
+    const {
+      body: { title, content, userId },
+    } = req;
+    if (!title || !content || !userId)
+      return res
+        .status(400)
+        .json({ error: "title, content, and userId are required" });
+    const post = await Post.create(req.body);
+    const user = await post.getUser();
+    post.dataValues.user = user;
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getPostById = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const post = await Post.findByPk(id, { include: User });
+    if (!post) return res.status(404).json({ error: "Post not found" });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const {
+      body: { title, content, userId },
+      params: { id },
+    } = req;
+    if (!title || !content || !userId)
+      return res
+        .status(400)
+        .json({ error: "title, content, and userId are required" });
+    const post = await Post.findByPk(id);
+    if (!post) return res.status(404).json({ error: "Post not found" });
+    await post.update(req.body);
+    const user = await post.getUser();
+    post.dataValues.user = user;
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const post = await Post.findByPk(id);
+    if (!post) return res.status(404).json({ error: "Post not found" });
+    await post.destroy();
+    res.json({ message: "Post deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ project-organisation/controllers/user.js
@@ -0,0 +1,72 @@
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const {
+      body: { firstName, lastName, email },
+    } = req;
+    if (!firstName || !lastName || !email)
+      return res
+        .status(400)
+        .json({ error: "firstName, lastName, and email are required" });
+    const found = await User.findOne({ where: { email } });
+    if (found) return res.status(400).json({ error: "User already exists" });
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const user = await User.findByPk(id, { include: Post });
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const {
+      body: { firstName, lastName, email },
+      params: { id },
+    } = req;
+    if (!firstName || !lastName || !email)
+      return res
+        .status(400)
+        .json({ error: "firstName, lastName, and email are required" });
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    await user.update(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    await user.destroy();
+    res.json({ message: "User deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
